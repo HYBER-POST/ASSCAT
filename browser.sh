7457f0b959339fc29311a7544fd982d34c075d4f
hyber-post <hyber.mega@gmail.com>
2026-05-03 01:53:14 +0700

    добавил User-Agent

--- browser.sh
+++ browser.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+VERSION="theta_2.13"
 cat="
-  /\_/\  
- ( o.o ) 
+  /\_/\
+ ( o.o )
   > ^ <"
 for ((1 ; ; 1)) do
   read -p "Введите url: " url
   if [[ "$url" == "exit" ]] then
     break
   fi
-  curl -L $url
+  curl -A "AssCat/$VERSION" -L "$url"
 done
